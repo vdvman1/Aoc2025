b7611551431943b424b28add4abca767c6b1aa8d
Stefan VDV <vdvman88@gmail.com>
2025-12-01 17:21:46 +1100

    feat: ✨ Solve day 1 part 1

---
+++ Aoc2025/Day01.cs
@@ -0,0 +1,46 @@
+﻿namespace Aoc2025;
+
+public partial class Day01 : DayBase
+{
+    /* 
+     * Measured performance:
+     * 
+     * 
+     */
+
+    [Benchmark]
+    public override void ParseData()
+    {
+        // No common parsing logic
+    }
+
+    [Benchmark]
+    public override string Solve1()
+    {
+        var parser = new Parser(Contents);
+
+        var dial = 50;
+        var zeroCount = 0;
+        while (!parser.IsEmpty)
+        {
+            var movement = parser.ParseOne() == 'L' ? -1 : 1;
+            movement *= parser.ParsePosInt();
+            
+            parser.MoveNext(); // Skip newline
+
+            dial = (dial + movement).FloorMod(100);
+            if (dial == 0)
+            {
+                zeroCount++;
+            }
+        }
+
+        return zeroCount.ToString();
+    }
+
+    [Benchmark]
+    public override string Solve2()
+    {
+        throw new NotImplementedException();
+    }
+}
