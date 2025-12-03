71cb382878c374c327b4651f2a428b7da7b29426
Stefan VDV <vdvman88@gmail.com>
2025-12-03 16:27:35 +1100

    feat: ✨ Solve day 3 part 1

---
+++ Aoc2025/Day03.cs
@@ -0,0 +1,43 @@
+﻿namespace Aoc2025;
+
+public partial class Day03 : DayBase
+{
+    public override void ParseData()
+    {
+        // No common parsing logic
+    }
+
+    public override string Solve1()
+    {
+        var parser = new Parser(Contents);
+        int total = 0;
+
+        while (parser.ParseLine() is { IsEmpty: false } line)
+        {
+            int max0 = line.ParseOne() - '0';
+            int max1 = -1;
+
+            while (line.TryParseDigit(out int digit))
+            {
+                if (digit > max0 && !line.IsEmpty)
+                {
+                    max0 = digit;
+                    max1 = -1;
+                }
+                else
+                {
+                    max1 = Math.Max(digit, max1);
+                }
+            }
+
+            total += 10 * max0 + max1;
+        }
+
+        return total.ToString();
+    }
+
+    public override string Solve2()
+    {
+        throw new NotImplementedException();
+    }
+}
