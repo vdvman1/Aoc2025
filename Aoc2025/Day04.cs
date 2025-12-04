30fbcbe8111c6b6ccd2ca28ae3f5476d23d692b4
Stefan VDV <vdvman88@gmail.com>
2025-12-04 17:47:47 +1100

    feat: ✨ Solve day 4 part 2

--- Aoc2025/Day04.cs
+++ Aoc2025/Day04.cs
@@ -26,10 +26,10 @@ public partial class Day04 : DayBase
 
         for (int y = 0; y < Grid.Count; y++)
         {
-            for (int x = 0; x < Grid[y].Length; x++)
+            var row = Grid[y];
+            for (int x = 0; x < row.Length; x++)
             {
-                var pos = new VectorI2d(x, y);
-                if (Grid.At(pos) && IsReachable(pos))
+                if (row[x] && IsReachable(x, y))
                 {
                     reachable++;
                 }
@@ -39,10 +39,10 @@ public partial class Day04 : DayBase
         return reachable.ToString();
     }
 
-    private bool IsReachable(VectorI2d pos)
+    private bool IsReachable(int x, int y)
     {
         int count = 0;
-        foreach (var (_, hasPaper) in Grid.Adjacent8(pos))
+        foreach (var (_, hasPaper) in Grid.Adjacent8(new(x, y)))
         {
             if (hasPaper)
             {
@@ -58,6 +58,65 @@ public partial class Day04 : DayBase
 
     public override string Solve2()
     {
-        throw new NotImplementedException();
+        // Copy grid to avoid mutating original
+        var grid = Grid.ConvertAll<bool[]>(row => [..row]);
+
+        int reachable = 0;
+        Stack<VectorI2d> locationsToCheck = new();
+
+        for (int y = 0; y < grid.Count; y++)
+        {
+            var row = grid[y];
+            for (int x = 0; x < row.Length; x++)
+            {
+                if (TryRemove(new(x, y), grid, locationsToCheck, includeAll: false))
+                {
+                    reachable++;
+                }
+            }
+        }
+
+        while (locationsToCheck.TryPop(out var startPos))
+        {
+            if (TryRemove(startPos, grid, locationsToCheck, includeAll: true))
+            {
+                reachable++;
+            }
+        }
+
+        return reachable.ToString();
+    }
+
+    private static bool TryRemove(VectorI2d startPos, List<bool[]> grid, Stack<VectorI2d> locationsToCheck, bool includeAll)
+    {
+        ref bool startCell = ref grid.RefAt(startPos);
+        if (!startCell) return false;
+
+        Span<VectorI2d> tempPotentialLocations = stackalloc VectorI2d[8];
+        int tempPotentialLocationCount = 0;
+
+        int count = 0;
+        foreach (var (pos, hasPaper) in grid.Adjacent8(startPos))
+        {
+            if (!hasPaper) continue;
+
+            ++count;
+            if (count >= 4) return false;
+
+            if (includeAll || pos.IsScannedEarlierThan(startPos))
+            {
+                tempPotentialLocations[tempPotentialLocationCount] = pos;
+                tempPotentialLocationCount++;
+            }
+        }
+
+        // This paper was reachable, so remove it and mark surrounding cells for future checking for reachability
+        startCell = false;
+        for (int i = 0; i < tempPotentialLocationCount; i++)
+        {
+            locationsToCheck.Push(tempPotentialLocations[i]);
+        }
+
+        return true;
     }
 }
