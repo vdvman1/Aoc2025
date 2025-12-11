9e7db88bc0b415d7e252df67b2e677120ea9e6a9
Stefan VDV <vdvman88@gmail.com>
2025-12-11 16:19:30 +1100

    feat: ✨ Add GridView as a simple wrapper around flat data to treat it as a grid

---
+++ Aoc2025/Utilities/GridView.cs
@@ -0,0 +1,18 @@
+﻿namespace Aoc2025.Utilities;
+
+public readonly ref struct GridView<T> where T : IEquatable<T>
+{
+    private readonly ReadOnlySpan<T> Data;
+
+    public readonly int Width;
+    public readonly int Height;
+
+    public GridView(ReadOnlySpan<T> data, T delimeter)
+    {
+        Data = data;
+        Width = data.IndexOf(delimeter);
+        Height = data.Count(delimeter);
+    }
+
+    public readonly T this[Index x, Index y] => Data[y.GetOffset(Height) * (Width + 1) + x.GetOffset(Width)];
+}
