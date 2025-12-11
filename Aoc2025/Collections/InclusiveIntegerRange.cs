dddf3233c3e0bf0d1cbef08ab483e9f758995d23
Stefan VDV <vdvman88@gmail.com>
2025-12-11 12:14:19 +1100

    feat: ✨ Add InclusiveIntegerRange

---
+++ Aoc2025/Collections/InclusiveIntegerRange.cs
@@ -0,0 +1,10 @@
+﻿using System.Numerics;
+
+namespace Aoc2025.Collections;
+
+public record struct InclusiveIntegerRange<T>(T Start, T End) where T : IBinaryInteger<T>
+{
+    public readonly bool Contains(T value) => Start <= value && value <= End;
+
+    public readonly T Count => End - Start + T.One;
+}
